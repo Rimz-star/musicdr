e5424ff1cdcca333002900f6be56a738089ef3fa
Rimzzzz <prc.rohim.gta@gmail.com>
2025-12-06 13:56:22 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import SongList from "./SongList";
+import Player from "./Player";
+
+function App() {
+  const [songs, setSongs] = useState([]);
+  const [current, setCurrent] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api/songs")
+      .then(res => res.json())
+      .then(data => setSongs(data));
+  }, []);
+
+  return (
+    <div style={{ display: "flex" }}>
+      <SongList songs={songs} onSelect={setCurrent} />
+      <Player song={current} />
+    </div>
+  );
+}
+
+export default App;
